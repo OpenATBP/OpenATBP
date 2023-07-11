a10f523a940242bf2e5168e95266f532f2c89fee
Darklit <0lies18pies@gmail.com>
2023-07-11 17:56:22 -0400

    Send pings to every team member and remove hard coding

--- game/src/main/java/xyz/openatbp/extension/reqhandlers/PingHandler.java
+++ game/src/main/java/xyz/openatbp/extension/reqhandlers/PingHandler.java
@@ -5,18 +5,25 @@ import com.smartfoxserver.v2.entities.data.ISFSObject;
 import com.smartfoxserver.v2.entities.data.SFSObject;
 import com.smartfoxserver.v2.extensions.BaseClientRequestHandler;
 import xyz.openatbp.extension.ATBPExtension;
+import xyz.openatbp.extension.RoomHandler;
+import xyz.openatbp.extension.game.actors.UserActor;
 
 public class PingHandler extends BaseClientRequestHandler {
     @Override
     public void handleClientRequest(User sender, ISFSObject params) {
         ATBPExtension parentExt = (ATBPExtension) getParentExtension();
-
-        ISFSObject data = new SFSObject();
-        data.putUtfString("id", String.valueOf(sender.getId()));
-        data.putInt("msg_type", params.getInt("msg_type"));
-        data.putFloat("x", params.getFloat("x"));
-        data.putFloat("y", params.getFloat("y"));
-        parentExt.send("cmd_mini_map_message", data, sender);
+        RoomHandler roomHandler = parentExt.getRoomHandler(sender.getLastJoinedRoom().getId());
+        UserActor user = roomHandler.getPlayer(String.valueOf(sender.getId()));
+        for(UserActor ua : roomHandler.getPlayers()){
+            if(ua.getTeam() == user.getTeam()){
+                ISFSObject data = new SFSObject();
+                data.putUtfString("id", String.valueOf(sender.getId()));
+                data.putInt("msg_type", params.getInt("msg_type"));
+                data.putFloat("x", params.getFloat("x"));
+                data.putFloat("y", params.getFloat("y"));
+                parentExt.send("cmd_mini_map_message", data, ua.getUser());
+            }
+        }
     }
 }
 
