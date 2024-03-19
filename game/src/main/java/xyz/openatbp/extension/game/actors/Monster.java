df0727f1ab923fe8b701c79b17f5bafe4749c7c2
Patgar1 <mateusz789852@gmail.com>
2024-03-19 19:45:56 +0100

    Change imports

--- game/src/main/java/xyz/openatbp/extension/game/actors/Monster.java
+++ game/src/main/java/xyz/openatbp/extension/game/actors/Monster.java
@@ -3,14 +3,20 @@ package xyz.openatbp.extension.game.actors;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.smartfoxserver.v2.SmartFoxServer;
 import com.smartfoxserver.v2.entities.Room;
-import xyz.openatbp.extension.*;
+import xyz.openatbp.extension.ATBPExtension;
+import xyz.openatbp.extension.ExtensionCommands;
+import xyz.openatbp.extension.MovementManager;
+import xyz.openatbp.extension.RoomHandler;
 import xyz.openatbp.extension.game.ActorState;
 import xyz.openatbp.extension.game.ActorType;
 import xyz.openatbp.extension.game.Champion;
 
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 public class Monster extends Actor {
